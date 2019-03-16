9340627b11f3e37e7e27e6a50975d33faedb8880
gautric <gautric@redhat.com>
2019-03-16 20:30:20 +0100

    add cst

---
+++ src/main/java/net/a/g/vertx/mqtt/util/MQTTConstantes.java
@@ -0,0 +1,8 @@
+package net.a.g.vertx.mqtt.util;
+
+public class MQTTConstantes {
+	public static final String HOST = "localhost";
+	public static final int PORT = 1883;
+	public static final String TOPIC = "topic";
+
+}
